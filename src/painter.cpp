1cfa79dbc3f824f5203115162fd876f33dfb4d13
Kim Wong <kimspindel@gmail.com>
2015-02-28 00:52:02 +0100

    added stroke file

--- src/painter.cpp
+++ src/painter.cpp
@@ -1,9 +1,27 @@
 #include "painter.hpp"
+#include "stroke.hpp"
 
-Painter::Painter()
+Painter::Painter()  :
+    gen(rd())
 {
 }
 
 void Painter::paint(const fea::Texture& original, fea::Texture& result, int n)
 {
+    std::uniform_int_distribution<> mColourRange(0, 255);
+    std::uniform_int_distribution<> mRadiusRange(5, 20);
+    std::uniform_int_distribution<> mPosXRange(0, original.getSize().x);
+    std::uniform_int_distribution<> mPosYRange(0, original.getSize().y);
+
+    for(int i = 0; i < n; ++i)
+    {
+        Stroke stroke(mColourRange(gen), mRadiusRange(gen), mPosXRange(gen), mPosYRange(gen));
+
+        for(int h = 0; h < (stroke.mRadius * 2); ++h)
+        {
+            for(int w = 0; w < (stroke.mRadius * 2); ++w)
+            {
+            }
+        }
+    }
 }
--- src/painter.hpp
+++ src/painter.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <fea/rendering/texture.hpp>
+#include <random>
+#include "stroke.hpp"
 
 class Painter
 {
@@ -8,4 +10,8 @@ class Painter
         void paint(const fea::Texture& original, fea::Texture& result, int n);
     
     private:
+        std::random_device rd;
+        std::mt19937 gen;
+
+        fea::Texture mCanvasTexture;
 };
