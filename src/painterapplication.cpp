71df8758558d078cc0e58d72780b1a0cc0e228d5
Tobias Widlund <widlundtobias@gmail.com>
2015-02-27 23:16:30 +0100

    Made it possible to quit and added a window

--- src/painterapplication.cpp
+++ src/painterapplication.cpp
@@ -1,13 +1,34 @@
 #include "painterapplication.hpp"
+#include <fea/ui/sdl2windowbackend.hpp>
+#include <fea/ui/sdl2inputbackend.hpp>
+
+PainterApplication::PainterApplication() :
+    mWindow(new fea::SDL2WindowBackend()),
+    mInputHandler(new fea::SDL2InputBackend())
+{
+}
 
 void PainterApplication::setup(const std::vector<std::string>& args)
 {
+    mWindow.create(fea::VideoMode(800, 600), "painter");
 }
 
 void PainterApplication::loop()
 {
+    fea::Event event;
+
+    while(mInputHandler.pollEvent(event))
+    {   
+        if(event.type == fea::Event::KEYPRESSED)
+        {
+            if(event.key.code == fea::Keyboard::ESCAPE)
+                quit();
+        }
+    }
+
 }
 
 void PainterApplication::destroy()
 {
+    mWindow.close();
 }
--- src/painterapplication.hpp
+++ src/painterapplication.hpp
@@ -1,10 +1,15 @@
 #pragma once
 #include <fea/structure.hpp>
+#include <fea/userinterface.hpp>
 
 class PainterApplication : public fea::Application
 {
     public:
+        PainterApplication();
         void setup(const std::vector<std::string>& args) override;
         void loop() override;
         void destroy() override;
+    private:
+        fea::Window mWindow;
+        fea::InputHandler mInputHandler;
 };
