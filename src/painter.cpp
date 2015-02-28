7bd1b9b0235034d289ff8d2f295480bcab7fcc8b
Tobias Widlund <widlundtobias@gmail.com>
2015-02-28 01:13:17 +0100

    Added prints

--- src/painter.cpp
+++ src/painter.cpp
@@ -1,5 +1,6 @@
 #include "painter.hpp"
 #include "stroke.hpp"
+#include <iostream>
 
 Painter::Painter()  :
     gen(rd())
@@ -8,10 +9,15 @@ Painter::Painter()  :
 
 void Painter::paint(const fea::Texture& original, fea::Texture& result, int n)
 {
+    std::cout << "Preparing to do " << n << " iterations! :)\n";
     std::uniform_int_distribution<> mColourRange(0, 255);
     std::uniform_int_distribution<> mRadiusRange(5, 20);
     std::uniform_int_distribution<> mPosXRange(0, original.getSize().x);
     std::uniform_int_distribution<> mPosYRange(0, original.getSize().y);
+
+    int32_t currentScore = calculateScore(result, original);
+
+    std::cout << "Starting score is " << currentScore << "\n";
 
     for(int i = 0; i < n; ++i)
     {
@@ -24,4 +30,9 @@ void Painter::paint(const fea::Texture& original, fea::Texture& result, int n)
             }
         }
     }
+}
+
+int32_t Painter::calculateScore(const fea::Texture& source, const fea::Texture& target) const
+{
+    return 0;
 }
