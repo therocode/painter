51a36f03c91c3ba6c5f8e16be9385bf75038d8dd
Kim Wong <kimspindel@gmail.com>
2015-02-28 13:30:18 +0100

    strokes now appear, not yet discarded based on score

--- src/painter.cpp
+++ src/painter.cpp
@@ -1,18 +1,19 @@
 #include "painter.hpp"
 #include "stroke.hpp"
 #include <iostream>
+#include <cmath>
 
 Painter::Painter()  :
     gen(rd())
 {
 }
 
-void Painter::paint(const fea::Texture& original, fea::Texture& result, int n)
+void Painter::paint(const fea::Texture& original, fea::Texture& result, int32_t n)
 {
     std::cout << "Preparing to do " << n << " iterations! :)\n";
     std::uniform_int_distribution<> mColourRange(0, 255);
     std::uniform_int_distribution<> mRadiusRange(5, 20);
-    std::uniform_int_distribution<> mPosXRange(0, original.getSize().x);
+    std::uniform_int_distribution<> mPosXRange(0, original.getSize().x);    // possibly extend these outside of the canvas border a bit
     std::uniform_int_distribution<> mPosYRange(0, original.getSize().y);
 
     // copy the result texture to a new, canvas texture for drawing on
@@ -22,30 +23,28 @@ void Painter::paint(const fea::Texture& original, fea::Texture& result, int n)
 
     std::cout << "Starting score is " << currentScore << "\n";
 
-    for(int i = 0; i < n; ++i)
+    for(int32_t i = 0; i < n; ++i)
     {
         fea::Color strokeColour(mColourRange(gen), mColourRange(gen), mColourRange(gen));
         Stroke stroke(strokeColour, mRadiusRange(gen), mPosXRange(gen), mPosYRange(gen));
 
-        // change to make h and w actually the pixel position //
-        for(int h = 0; h < (stroke.mRadius * 2); ++h)
+        int32_t wStart = std::max(stroke.mPosX - stroke.mRadius, 0);
+        int32_t hStart = std::max(stroke.mPosY - stroke.mRadius, 0);
+        int32_t wLimit = std::min(stroke.mPosX + stroke.mRadius, int32_t(mCanvasTexture.getSize().x) - 1);
+        int32_t hLimit = std::min(stroke.mPosY + stroke.mRadius, int32_t(mCanvasTexture.getSize().y) - 1);
+
+        for(int32_t h = hStart; h < hLimit; ++h)
         {
-            for(int w = 0; w < (stroke.mRadius * 2); ++w)
+            for(int32_t w = wStart; w < wLimit; ++w)
             {
                 float deltaX = abs(stroke.mPosX - w);
                 float deltaY = abs(stroke.mPosY - h);
 
-                float distanceFromCentre = sqrt((deltaX * deltaX) + (deltaY * deltaY));
+                float distanceFromCentre = std::hypot(deltaX, deltaY);
                 float distanceAsDecimal = distanceFromCentre / float(stroke.mRadius);
-                int transparency = int(lerp(0.0f, 255.0f, distanceAsDecimal));
-
-                // unnecessary when h and w pixel pos, do up there then //
-                int xalle = std::max(std::min(stroke.mPosX - stroke.mRadius + w, int(mCanvasTexture.getSize().x) - 1), 0);
-                int yalle = std::max(std::min(stroke.mPosY - stroke.mRadius + h, int(mCanvasTexture.getSize().y) - 1), 0);
-
-                fea::Color pixelColour = stroke.mColour;
-                pixelColour.setA(transparency);
-                mCanvasTexture.setPixel(xalle, yalle, pixelColour);
+                fea::Color pixelColour = lerpColour(stroke.mColour, mCanvasTexture.getPixel(w, h), distanceAsDecimal);
+                
+                mCanvasTexture.setPixel(w, h, pixelColour);
             }
         }
     }
@@ -54,12 +53,18 @@ void Painter::paint(const fea::Texture& original, fea::Texture& result, int n)
     result.create(result.getSize().x, result.getSize().y, mCanvasTexture.getPixelData(), false, true);
 }
 
-float Painter::lerp(float a, float b, float amount)
+fea::Color Painter::lerpColour(const fea::Color& a, const fea::Color& b, float amount)
 {
     amount = std::max(std::min(amount, 1.0f), 0.0f);
-    float difference = b - a;
-    float position = difference * amount;
-    return (a + position);
+    float deltaR = int32_t(b.r()) - int32_t(a.r());
+    float deltaG = int32_t(b.g()) - int32_t(a.g());
+    float deltaB = int32_t(b.b()) - int32_t(a.b());
+
+    uint8_t red   = a.r() + (deltaR * amount);
+    uint8_t green = a.g() + (deltaG * amount);
+    uint8_t blue  = a.b() + (deltaB * amount);
+
+    return fea::Color(red, green, blue);
 }
 
 int32_t Painter::calculateScore(const fea::Texture& source, const fea::Texture& target) const
